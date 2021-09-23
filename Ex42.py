d6095afc580cad18dabb22b7d38622567e0ac24a
AbdMamun94 <91117135+AbdMamun94@users.noreply.github.com>
2021-09-23 21:42:40 +0200

    Ex 34, 42

---
+++ Ex42.py
@@ -0,0 +1,10 @@
+import numpy as np
+x = np.random.randint(0,3,6)
+print("First array:")
+print(x)
+y = np.random.randint(0,2,6)
+print("Second array:")
+print(y)
+print("Test above two arrays are equal !")
+array_equal = np.allclose(x, y)
+print(array_equal)
---
+++ ex34.py
@@ -0,0 +1,3 @@
+import numpy as np
+print("july, 2016")
+print(np.arange('2016-07', '2016-08', dtype='datetime64[D]'))
